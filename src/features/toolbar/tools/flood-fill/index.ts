182f746243d49f8e6057fca1fab47d3d78e6d250
Alex Davis <alexd41@gmail.com>
2024-06-02 13:54:53 +0100

    remove flood fill index.ts

--- src/features/toolbar/tools/flood-fill/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './flood-fill.component';
--- src/features/toolbar/tools/index.ts
+++ src/features/toolbar/tools/index.ts
@@ -1,3 +1,3 @@
 export * from './brush-control/brush-control.component';
 export * from './colour-selector/colour-selector.component';
-export * from './flood-fill';
+export * from './flood-fill/flood-fill.component';
